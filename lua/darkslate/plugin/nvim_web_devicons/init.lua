c4317fa5f99872f51387f08d3676661da7283c8a
SomeCoder99 <r1zone.el4nsy4h@gmail.com>
2025-04-25 18:58:38 +0700

    added options tweak for nvim-web-devicons icon color by filename

--- lua/darkslate/plugin/nvim_web_devicons/init.lua
+++ lua/darkslate/plugin/nvim_web_devicons/init.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.tweak_opts(opts)
-  opts.override_by_extension = {}
+  opts.override_by_extension = opts.override_by_extension or {}
   local file_extension = require("darkslate.plugin.nvim_web_devicons.file_extension")
   local orig_file_extension = require("nvim-web-devicons.default.icons_by_file_extension")
   for ext, color in pairs(file_extension) do
@@ -11,6 +11,18 @@ function M.tweak_opts(opts)
       color = color,
     }
   end
+
+  opts.override_by_filename = opts.override_by_filename or {}
+  local filename = require("darkslate.plugin.nvim_web_devicons.filename")
+  local orig_filename = require("nvim-web-devicons.default.icons_by_filename")
+  for fname, color in pairs(filename) do
+    opts.override_by_filename[fname] = {
+      icon = orig_filename[fname].icon,
+      name = orig_filename[fname].name,
+      color = color,
+    }
+  end
+
   return opts
 end
 
