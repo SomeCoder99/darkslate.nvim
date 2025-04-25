50ed26f284552e9ae6f950bfec87019d25b3f0fb
SomeCoder99 <r1zone.el4nsy4h@gmail.com>
2025-04-25 19:46:02 +0700

    added options tweak for nvim-web-devicons icon color by operating system, desktop environment and window manager

---
+++ lua/darkslate/plugin/nvim_web_devicons/desktop_environment.lua
@@ -0,0 +1,10 @@
+return {
+  ["budgie"]   = "#292b2e", -- orig: #4E5361
+  ["cinnamon"] = "#63331b", -- orig: #DC682E
+  ["gnome"]    = "#6b6b6b", -- orig: #FFFFFF
+  ["lxde"]     = "#3b3b3b", -- orig: #A4A4A4
+  ["lxqt"]     = "#0e445c", -- orig: #0191D2
+  ["mate"]     = "#526e37", -- orig: #9BDA5C
+  ["plasma"]   = "#14314d", -- orig: #1B89F4
+  ["xfce"]     = "#0e4f63", -- orig: #00AADF
+}
--- lua/darkslate/plugin/nvim_web_devicons/init.lua
+++ lua/darkslate/plugin/nvim_web_devicons/init.lua
@@ -23,6 +23,39 @@ function M.tweak_opts(opts)
     }
   end
 
+  opts.override_by_operating_system = opts.override_by_operating_system or {}
+  local operating_system = require("darkslate.plugin.nvim_web_devicons.operating_system")
+  local orig_operating_system = require("nvim-web-devicons.default.icons_by_operating_system")
+  for os, color in pairs(operating_system) do
+    opts.override_by_operating_system[os] = {
+      icon = orig_operating_system[os].icon,
+      name = orig_operating_system[os].name,
+      color = color,
+    }
+  end
+
+  opts.override_by_desktop_environment = opts.override_by_desktop_environment or {}
+  local desktop_environment = require("darkslate.plugin.nvim_web_devicons.desktop_environment")
+  local orig_desktop_environment = require("nvim-web-devicons.default.icons_by_desktop_environment")
+  for de, color in pairs(desktop_environment) do
+    opts.override_by_desktop_environment[de] = {
+      icon = orig_desktop_environment[de].icon,
+      name = orig_desktop_environment[de].name,
+      color = color,
+    }
+  end
+
+  opts.override_by_window_manager = opts.override_by_window_manager or {}
+  local window_manager = require("darkslate.plugin.nvim_web_devicons.window_manager")
+  local orig_window_manager = require("nvim-web-devicons.default.icons_by_window_manager")
+  for wm, color in pairs(window_manager) do
+    opts.override_by_window_manager[wm] = {
+      icon = orig_window_manager[wm].icon,
+      name = orig_window_manager[wm].name,
+      color = color,
+    }
+  end
+
   return opts
 end
 
