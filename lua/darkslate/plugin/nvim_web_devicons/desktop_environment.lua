7a25e6f3ff8073584e686e03dc5d7c59ead16ee5
SomeCoder99 <r1zone.el4nsy4h@gmail.com>
2025-04-27 06:29:39 +0700

    added a brighter version for nvim-web-devicon color

--- lua/darkslate/plugin/nvim_web_devicons/desktop_environment.lua
+++ lua/darkslate/plugin/nvim_web_devicons/desktop_environment.lua
@@ -1,10 +1,10 @@
 return {
-  ["budgie"]   = { "#292b2e" }, -- orig: #4E5361
-  ["cinnamon"] = { "#63331b" }, -- orig: #DC682E
-  ["gnome"]    = { "#6b6b6b" }, -- orig: #FFFFFF
-  ["lxde"]     = { "#3b3b3b" }, -- orig: #A4A4A4
-  ["lxqt"]     = { "#0e445c" }, -- orig: #0191D2
-  ["mate"]     = { "#526e37" }, -- orig: #9BDA5C
-  ["plasma"]   = { "#14314d" }, -- orig: #1B89F4
-  ["xfce"]     = { "#0e4f63" }, -- orig: #00AADF
+  ["budgie"]   = { "#292b2e", "#373a42" }, -- orig: #4E5361
+  ["cinnamon"] = { "#63331b", "#994e28" }, -- orig: #DC682E
+  ["gnome"]    = { "#6b6b6b", "#8c8c8c" }, -- orig: #FFFFFF
+  ["lxde"]     = { "#3b3b3b", "#6e6e6e" }, -- orig: #A4A4A4
+  ["lxqt"]     = { "#0e445c", "#06648f" }, -- orig: #0191D2
+  ["mate"]     = { "#526e37", "#77a84a" }, -- orig: #9BDA5C
+  ["plasma"]   = { "#14314d", "#18608d" }, -- orig: #1B89F4
+  ["xfce"]     = { "#0e4f63", "#0880a3" }, -- orig: #00AADF
 }
--- lua/darkslate/plugin/nvim_web_devicons/window_manager.lua
+++ lua/darkslate/plugin/nvim_web_devicons/window_manager.lua
@@ -1,14 +1,14 @@
 return {
-  ["awesomewm"]     = { "#2b2727" }, -- orig: #535D6C
-  ["bspwm"]         = { "#212326" }, -- orig: #4F4F4F
-  ["dwm"]           = { "#0e364a" }, -- orig: #1177AA
-  ["enlightenment"] = { "#707070" }, -- orig: #FFFFFF
-  ["fluxbox"]       = { "#2e2e2e" }, -- orig: #555555
-  ["hyprland"]      = { "#0a484a" }, -- orig: #00AAAE
-  ["i3"]            = { "#525354" }, -- orig: #E8EBEE
-  ["jwm"]           = { "#0b3552" }, -- orig: #0078CD
-  ["qtile"]         = { "#6e6e6e" }, -- orig: #FFFFFF
-  ["river"]         = { "#1d1d1d" }, -- orig: #000000
-  ["sway"]          = { "#303610" }, -- orig: #68751C
-  ["xmonad"]        = { "#87323a" }, -- orig: #FD4D5D
+  ["awesomewm"]     = { "#2b2727", "#3d4752" }, -- orig: #535D6C
+  ["bspwm"]         = { "#212326", "#3b3b3b" }, -- orig: #4F4F4F
+  ["dwm"]           = { "#0e364a", "#105b80" }, -- orig: #1177AA
+  ["enlightenment"] = { "#707070", "#949494" }, -- orig: #FFFFFF
+  ["fluxbox"]       = { "#2e2e2e", "#404040" }, -- orig: #555555
+  ["hyprland"]      = { "#0a484a", "#046e70" }, -- orig: #00AAAE
+  ["i3"]            = { "#525354", "#737475" }, -- orig: #E8EBEE
+  ["jwm"]           = { "#0b3552", "#045590" }, -- orig: #0078CD
+  ["qtile"]         = { "#6e6e6e", "#808080" }, -- orig: #FFFFFF
+  ["river"]         = { "#1d1d1d", "#222222" }, -- orig: #000000
+  ["sway"]          = { "#303610", "#4d5717" }, -- orig: #68751C
+  ["xmonad"]        = { "#87323a", "#ad424c" }, -- orig: #FD4D5D
 }
